6356cc0f6795b18bcfd004d244723695a14b7e81
Pedro Felipe Onofre Utumi <130059527+PedrinnhoUtumi@users.noreply.github.com>
2024-06-28 11:27:32 -0300

    ativ

---
+++ src/script.js
@@ -0,0 +1,11 @@
+const user = document.getElementById("user")
+const add = document.getElementById("adicionar")
+const bg = document.getElementById("plano_fundo")
+const profile = document.getElementById("perfil")
+const name = document.getElementById("nome_user")
+const email = document.getElementById("email_user")
+
+
+add.addEventListener("click", () => {
+    
+})
