e7cce9d17e45b343a12fd7fde11303ad2e14d2b0
Pedro Felipe Onofre Utumi <130059527+PedrinnhoUtumi@users.noreply.github.com>
2024-07-04 22:34:33 -0300

    atividade cartoes

--- src/script.js
+++ src/script.js
@@ -1,11 +1,34 @@
 const user = document.getElementById("user")
+const card = document.getElementById("cartao")
 const add = document.getElementById("adicionar")
 const bg = document.getElementById("plano_fundo")
 const profile = document.getElementById("perfil")
-const name = document.getElementById("nome_user")
+const name_user = document.getElementById("nome_user")
 const email = document.getElementById("email_user")
+const list_repo = document.getElementById("repo_list")
 
-
-add.addEventListener("click", () => {
-    
+add.addEventListener("click", async () => {
+    try {
+        const response = await axios.get(`https://api.github.com/users/${user.value}`)
+        let info = response.data
+        console.log(info)
+        let name_git = info.name
+        let email_git = info.email
+        let new_profile = info.avatar_url
+        name_user.textContent = name_git
+        email.textContent = email_git
+        profile.src = new_profile
+        const repository = await axios.get(`https://api.github.com/users/${user.value}/repos`)
+        let repo = repository.data 
+        console.log(repo)
+        list_repo.innerHTML = ""
+        repo.forEach(rep => {
+            const li = document.createElement("li")
+            li.style.listStyle = "none"
+            li.textContent = rep.name
+            list_repo.appendChild(li)
+        })
+    } catch (error) {
+        console.error("Perfil não encontrado:", error)
+    }
 })
